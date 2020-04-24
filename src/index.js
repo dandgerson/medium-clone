4326ad4ca01a65b82322af0bd9584cc4f4fc2b08
Dmitry G. Anderson <dandgerson@gmail.com>
2020-04-24 18:17:15 +0300

    install router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom'
+
+import Routes from 'routes'
+
+const App = () => (
+  <div>
+    <div><span>Welcome to hooks</span></div>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </div>
+)
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +20,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
